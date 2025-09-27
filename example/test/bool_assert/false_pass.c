3c808921a5db456a3099503cad529cd8695cfb67
xDec0de <xdec0deyt@gmail.com>
2025-09-27 10:33:29 +0200

    Remove old bool tests

--- example/test/bool_assert/false_pass.c
+++
@@ -1,8 +0,0 @@
-#include "cst_assertions.h"
-#include "cst_example.h"
-
-int	main(void)
-{
-	CST_TEST_NAME = "cst_isnum('1') == true";
-	ASSERT_TRUE(cst_isnum('1'));
-}
--- example/test/bool_assert/true_fail.c
+++
@@ -1,8 +0,0 @@
-#include "cst_assertions.h"
-#include "cst_example.h"
-
-int	main(void)
-{
-	CST_TEST_NAME = "cst_isnum('a') == true";
-	ASSERT_TRUE(cst_isnum('a'));
-}
