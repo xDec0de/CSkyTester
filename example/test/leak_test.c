6470c4bb93b931ef867860d038183e54c75361d5
xDec0de <xdec0deyt@gmail.com>
2025-09-27 19:03:47 +0200

    Added note to force memory leak test

--- example/test/leak_test.c
+++ example/test/leak_test.c
@@ -1,5 +1,6 @@
 #include "cst_assertions.h"
 
-TEST("Memory tests", "Force memory leak") {
+TEST("Memory tests", "Force memory leak (Shouldn't pass)") {
 	char *ptr = malloc(42);
+	ASSERT_NULL(NULL);
 }
