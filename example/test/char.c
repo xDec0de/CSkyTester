20d6150b6cab53135fc706db20866f6a1691d08a
xDec0de <xdec0deyt@gmail.com>
2025-09-27 13:36:28 +0200

    Added char tests

---
+++ example/test/char.c
@@ -0,0 +1,24 @@
+#include "cst_assertions.h"
+#include "cst_example.h"
+
+static const char *category = "Char assertions";
+
+// Char
+
+TEST(category, "cst_toupper('a') == 'B'") {
+	ASSERT_CHAR_EQUALS(cst_toupper('a'), 'B');
+}
+
+TEST(category, "cst_toupper('a') == 'A'") {
+	ASSERT_CHAR_EQUALS(cst_toupper('a'), 'A');
+}
+
+// Unsigned char
+
+TEST(category, "cst_utoupper('a') == 'B'") {
+	ASSERT_UCHAR_EQUALS(cst_utoupper((unsigned char) 'a'), (unsigned char) 'B');
+}
+
+TEST(category, "cst_utoupper('a') == 'A'") {
+	ASSERT_UCHAR_EQUALS(cst_utoupper((unsigned char) 'a'), (unsigned char) 'A');
+}
